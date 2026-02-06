c765afd1521aea435a3c46c5625b2382f5a13f69
yohanlb <yohan.lbtn@proton.me>
2026-02-06 22:01:52 +0100

    chore: Update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/sudoku-generator/",
   server: {
     port: 3000,
     open: true,
